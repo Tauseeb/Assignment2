d213150faf358b4f8228ba7117b297e75a7bd80c
tausib TIPL <tausib TIPL@DESKTOP-N3BL7T3>
2022-09-23 13:03:10 +0530

    Add project files.

---
+++ Lab2/CompCmd/CommentViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Text.Json;
+
+namespace Lab2.Components
+{
+    public class CommentViewComponent : ViewComponent
+    {
+        public const string comment = "Comments";
+
+        public IViewComponentResult Invoke()
+        {
+            string sessionString = HttpContext.Session.GetString("Comments");
+            List<string> comments = new List<string>();
+            if (!string.IsNullOrWhiteSpace(sessionString))
+            {
+                comments = JsonSerializer.Deserialize<List<string>>(sessionString);
+            }
+
+            return View("~/views/Comp/_commentss.cshtml", comments);
+        }
+    }
+}
